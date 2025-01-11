acfd318bebffdb6eb06e90c63a5b7f19a8349d25
Andrya Dornelles <andryacds@gmail.com>
2025-01-11 12:50:10 -0300

    UPDATE: CreateDatabase sql

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -14,10 +14,61 @@ CREATE TABLE [Material] (
     [QuantidadeEstoque] INT NOT NULL,
     [QuantidadeNecessaria] INT NOT NULL
 
-    CONSTRAINT [PK_Material] PRIMARY KEY([Id])
+    CONSTRAINT [PK_Material] PRIMARY KEY ([Id])
     CONSTRAINT [FK_Material_Fornecedor] FOREIGN KEY ([FornecedorId])
         REFERENCES [Fornecedor]([Id])
 )
+
+CREATE TABLE [Medico] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    
+    CONSTRAINT [PK_Medico] PRIMARY KEY ([Id])
+)
+
+CREATE TABLE [Paciente] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Nome] NVARCHAR(120) NOT NULL,
+    [Telefone] NVARCHAR(20) NOT NULL,
+    [Convenio] CHAR(2) NOT NULL,
+    [MedicoId] UNIQUEIDENTIFIER NOT NULL
+
+    CONSTRAINT [PK_Paciente] PRIMARY KEY ([Id])
+    CONSTRAINT [FK_Paciente_Medico] FOREIGN KEY ([MedicoId])
+        REFERENCES [Medico]([Id])
+)
+
+CREATE TABLE [Procedimento] (
+    [Id] INT NOT NULL IDENTITY(300,1),
+    [Nome] NVARCHAR(80) NOT NULL,
+    [Descricao] TEXT
+
+    CONSTRAINT [PK_Procedimento] PRIMARY KEY ([Id])
+)
+
+CREATE TABLE [ProcedimentoMaterial] (
+    [Id] INT NOT NULL IDENTITY(500,1),
+    [MaterialId] INT NOT NULL,
+    [Quantidade] NVARCHAR(4) NOT NULL,
+    [Custo] DECIMAL(10,2) NOT NULL
+)
+
+CREATE TABLE [ProcedimentoRealizado] (
+    [Id] INT NOT NULL IDENTITY(400,1),
+    [ProcedimentoId] INT NOT NULL,
+    [Data] DATETIME NOT NULL,
+    [PacienteId] UNIQUEIDENTIFIER NOT NULL,
+    [MedicoId] UNIQUEIDENTIFIER NOT NULL
+
+    CONSTRAINT [PK_ProcedimentoRealizado] PRIMARY KEY
+    CONSTRAINT [FK_ProcedimentoRealizado_Paciente] FOREIGN KEY ([PacienteId])
+        REFERENCES [Paciente]([Id]),
+    CONSTRAINT [FK_ProcedimentoRealizado_Medico] FOREIGN KEY ([MedicoId])
+        REFERENCES [Medico]([Id])
+)
+
+ALTER TABLE [Material]
+    ADD [Preco] DECIMAL(10,2) NOT NULL
 
 ALTER TABLE [Fornecedor]
     ADD [Nome] VARCHAR(80) NOT NULL
