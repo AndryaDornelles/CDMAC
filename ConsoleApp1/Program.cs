644381cfa2afc58831c7a8191692d0343b785378
Andrya Dornelles <andryacds@gmail.com>
2025-01-25 00:26:05 -0300

    add: teste index.html and img

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String de conexao
+            const string connectionString = "Server=127.0.0.1,1433;Database=Balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=true";
+            // using cria um bloco de código que é executado e depois descartado
+            using (var connection = new SqlConnection(connectionString))
+            {
+                // query faz a consulta no banco de dados em lista de categorias, usando comando sql
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                // foreach faz um loop em cada categoria
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} - {category.Title}");
+                }
+            }
+        }
+    }
+}
